8e2d0d29161ea8378fa8a16861021b4ab469371d
Euan Ong <euan.l.y.ong@gmail.com>
2020-11-04 14:57:37 +0000

    Ultrasonic sensor

--- test/ultrasonictest.ino
+++ test/ultrasonictest.ino
@@ -1,33 +1,38 @@
 
 
-const int pingPin = 12; // Trigger Pin of Ultrasonic Sensor
-const int echoPin = 13; // Echo Pin of Ultrasonic Sensor
+const int us_left_trigger = 14;
+const int us_left_echo = 15;
+const int us_centre_trigger = 12;
+const int us_centre_echo = 13;
+const int us_right_trigger = 2;
+const int us_right_echo = 3;
 
 void setup() {
    Serial.begin(9600); // Starting Serial Terminal
+   pinMode(us_left_trigger, OUTPUT);
+   pinMode(us_left_echo, INPUT);
+   pinMode(us_centre_trigger, OUTPUT);
+   pinMode(us_centre_echo, INPUT);
+   pinMode(us_right_trigger, OUTPUT);
+   pinMode(us_right_echo, INPUT);
 }
 
 void loop() {
-   long duration, inches, cm;
-   pinMode(pingPin, OUTPUT);
-   digitalWrite(pingPin, LOW);
-   delayMicroseconds(2);
-   digitalWrite(pingPin, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(pingPin, LOW);
-   pinMode(echoPin, INPUT);
-   duration = pulseIn(echoPin, HIGH);
-   cm = microsecondsToCentimeters(duration);
-   Serial.print(cm);
-   Serial.print("cm");
+   Serial.print(ultrasonic_read(us_left_trigger, us_left_echo));
+   Serial.print(" ");
+   Serial.print(ultrasonic_read(us_centre_trigger, us_centre_echo));
+   Serial.print(" ");
+   Serial.print(ultrasonic_read(us_right_trigger, us_right_echo));
    Serial.println();
    delay(100);
 }
 
-long read_distance(){
-  
-}
-
-long microsecondsToCentimeters(long microseconds) {
-   return microseconds / 29 / 2;
+double ultrasonic_read(int ping, int echo){
+   digitalWrite(ping, LOW);
+   delayMicroseconds(2);
+   digitalWrite(ping, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(ping, LOW);
+   pinMode(echo, INPUT);
+   return pulseIn(echo, HIGH) / 29.0 / 2.0; // in centimetres
 }
